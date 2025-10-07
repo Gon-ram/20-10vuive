d9a5c1562d7912a15bebb31be799c030bdc7080c
Gon-ram <datgoodboy0508@gmail.com>
2025-10-07 18:31:53 +0700

    cập nhật

--- hachi.js
+++ hachi.js
@@ -819,7 +819,7 @@ let galaxyAudio = null;
 
 function preloadGalaxyAudio() {
   const audioSources = [
-   "phepmau.mp3.mp3"
+   "suongmaitrongveo.mp3"
   ];
 
   const randomIndex = Math.floor(Math.random() * audioSources.length);
