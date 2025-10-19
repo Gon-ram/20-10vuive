fc52be0b5b9ffadd317b742840886405733b6b4b
Gon-ram <datgoodboy0508@gmail.com>
2025-10-19 16:44:20 +0700

    cập nhật toàn bộ thay đổi

--- vananh.js
+++ vananh.js
@@ -84,7 +84,7 @@ const galaxyParameters = {
   outsideColor: new THREE.Color(0x48b8b8),
 };
 
-const defaultHeartImages = Array.from({ length: 9 }, (_, i) => `imageva/va${i + 1}.jpg`);
+const defaultHeartImages = Array.from({ length: 9 }, (_, i) => `imageva/c${i + 1}.jpg`);
 
 const heartImages = [
   ...(window.dataCCD?.data?.heartImages || []),
