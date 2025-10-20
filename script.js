0acb66a82d592355686a5bf755f3eae9a54aef87
Gon-ram <datgoodboy0508@gmail.com>
2025-10-20 11:56:59 +0700

    cập nhật toàn bộ thay đổi

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Danh sách 18 tài khoản + ánh xạ username -> file HTML riêng
 const accounts = [
   { username: "Ngọc Bảo",  password: "1208",  page: "ngocbao.html"  },
-  { username: "Trang Linh",  password: "2411",  page: "tranglinh.html"  },
+  { username: "Trang Linh",  password: "2411",  page: "a.html"  },
   { username: "Vân Anh",  password: "0707",  page: "vananh.html"  },
   { username: "Thu Trang",  password: "1409",  page: "thutrang.html"  },
   { username: "Ngọc Hà",  password: "2704",  page: "ngha.html"  },
